8ea83ffae48c202394c2524ed23ae35ffb7bd963
KevTheDev34 <k.ramirez.dev@gmail.com>
2025-08-07 17:33:34 -0700

    Created html, css, and js files. Following tutorial to create game.

---
+++ assets/scripts/main.js
@@ -0,0 +1,15 @@
+class Board {
+    constructor(canvas, context) {
+        this.canvas = canvas;
+        this.context = context;
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+    }
+}
+
+window.addEventListener('load', function() {
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 720;
+    canvas.height = 720;
+});
