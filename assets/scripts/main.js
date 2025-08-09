d909f069e26ad5afe9fb1e18749d0ba5e22b794a
KevTheDev34 <k.ramirez.dev@gmail.com>
2025-08-08 22:59:58 -0700

    organized code a bit better, created a tile class and filled the blank board

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,15 +1,51 @@
+import Tile from './tile.js';
 class Board {
     constructor(canvas, context) {
         this.canvas = canvas;
-        this.context = context;
+        this.ctx = context;
         this.width = this.canvas.width;
         this.height = this.canvas.height;
+        this.tiles = []; // 4x4 grid
+        this.tileSpacing = 25; // space between tiles
+        this.tileSize = (this.width - (this.tileSpacing * 5)) / 4; // calculate tile size based on canvas width and spacing
+        this.initTiles();
+    }
+
+    render() {
+    }
+
+    initTiles() {
+        for (let i = 0; i < 4; i++) {
+            this.tiles[i] = [];
+            for (let j = 0; j < 4; j++) {
+                this.tiles[i][j] = new Tile(0); // initialize all tiles to 0
+            }
+        }
+        console.log(this.tiles);
+        this.drawBoard();
+    }
+
+    drawBoard() {
+        this.ctx.clearRect(0, 0, this.width, this.height);
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                const tile = this.tiles[i][j];
+                this.ctx.fillStyle = tile.color;
+                const x = j * (this.tileSize + this.tileSpacing) + this.tileSpacing;
+                const y = i * (this.tileSize + this.tileSpacing) + this.tileSpacing;
+                this.ctx.fillRect(x, y, this.tileSize, this.tileSize);
+            }
+        }
     }
 }
 
-window.addEventListener('load', function() {
-    const canvas = document.getElementById('canvas1');
+window.addEventListener('load', function(){
+    const canvas = this.document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
-    canvas.width = 720;
-    canvas.height = 720;
+    canvas.width = 500;
+    canvas.height = 500;
+
+    const board = new Board(canvas, ctx);
+    board.render();
+    console.log(board.tiles)
 });
---
+++ assets/scripts/tile.js
@@ -0,0 +1,27 @@
+
+class Tile {
+    constructor(value) {
+        this.value = value;
+        this.color = Tile.getColor(value);
+    }
+
+    static getColor(value) {
+        const colors = {
+            0: '#d0bfae',      // none
+            2: '#eee4da',
+            4: '#ede0c8',
+            8: '#f2b179',
+            16: '#f59563',
+            32: '#f67c5f',
+            64: '#f65e3b',
+            128: '#edcf72',
+            256: '#edcc61',
+            512: '#edc850',
+            1024: '#edc53f',
+            2048: '#edc22e'
+        };
+        return colors[value] || '#3c3a32'; // fallback for higher values
+    }
+}
+
+export default Tile;
