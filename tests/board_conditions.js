c849427ac1a95d7a5d290fd582324e4bd460024a
KevTheDev34 <k.ramirez.dev@gmail.com>
2025-08-16 16:19:43 -0700

    edited merge function to go traverse board from the bottom up, and edited slideDown function to slide pieces down, merge any corresponding pieces correctly, and slide down again to get rid of gaps

--- tests/board_conditions.js
+++ tests/board_conditions.js
@@ -2,7 +2,6 @@ const Board = require('../assets/scripts/main.js');
 
 function stackedCombination() {
     const board = new Board(document.createElement('canvas'), document.createElement('canvas').getContext('2d'));
-    
     // Clear all tiles to 0 tiles, because creating a new board will automatically set 2 tiles to 2
     for (let i = 0; i < 4; i++) {
         for (let j = 0; j < 4; j++) {
@@ -13,8 +12,9 @@ function stackedCombination() {
     board.setTile(0, 0, 2);
     board.setTile(1, 0, 2);
     board.setTile(2, 0, 2);
+    board.printBoard(); // Print initial board state
     board.tilt("down"); // Slide down
-
+    board.printBoard(); // Print board state after sliding down
     return board.tiles;
 
 }
